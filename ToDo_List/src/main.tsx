b6d9929f480696a2e618b360a193ed0e9fbe4969
Sulagna Sinha <sulagna_sinha@epam.com>
2025-07-31 13:30:44 +0530

    updated App.tsx with basic text

--- ToDo_List/src/main.tsx
+++ ToDo_List/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import  App_renamed from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* with default exports we can change the import name of the component. */}
+    <App_renamed />
   </StrictMode>,
 )
